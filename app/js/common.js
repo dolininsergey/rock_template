f2d97473115eeab6f6e50f86b4859bb0362d5486
Sergey <code.sergey.dolinin@gmail.com>
2017-09-26 01:44:25 +0300

    ---

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,41 @@
 $(function() {
 
-// To be Done like in this example
-// https://learn.javascript.ru/mousemove-mouseover-mouseout-mouseenter-mouseleave#события-mouseenter-и-mouseleave
+			$('#musician-1 .member-description').hover(function() {
+					$('#musician-1 .mem-socials').css('display', 'block');
+			}, function() {
+					$('#musician-1 .mem-socials').css('display', 'none');
+			});
+
+			$('#musician-2 .member-description').hover(function() {
+					$('#musician-2 .mem-socials').css('display', 'block');
+			}, function() {
+					$('#musician-2 .mem-socials').css('display', 'none');
+			});
+
+			$('#musician-3 .member-description').hover(function() {
+					$('#musician-3 .mem-socials').css('display', 'block');
+			}, function() {
+					$('#musician-3 .mem-socials').css('display', 'none');
+			});			
+
+			$('.members-carousel').owlCarousel({
+					loop: true, 
+					nav: true,
+					navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+					items: 1,
+			});
+
+			$('.concerts-carousel').owlCarousel({
+					loop: true,
+					nav: true,
+					navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+					items: 1,					
+			});
+			
+
+
+			//preloader is here
+			$('.preloader').fadeOut();
+
 
 });
