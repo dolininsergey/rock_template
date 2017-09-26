fd14bed21cf18c5228b68531a07a7602666832c8
Sergey <code.sergey.dolinin@gmail.com>
2017-09-26 14:58:21 +0300

    audio playes is added

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,8 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/owl.carousel/dist/owl.carousel.min.js',
+		'app/libs/audio-player/plugin/jquery-jplayer/jquery.jplayer.js',
+		'app/libs/audio-player/plugin/ttw-music-player-min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
