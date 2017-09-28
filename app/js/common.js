52a9ecc1a6c42c485c1dae27c0bb8ddedcf01299
Sergey <code.sergey.dolinin@gmail.com>
2017-09-28 19:38:56 +0300

    ---

--- app/js/common.js
+++ app/js/common.js
@@ -1,39 +1,38 @@
 $(function() {
 
-			$('#musician-1 .member-description').hover(function() {
-					$('#musician-1 .mem-socials').css('display', 'block');
-			}, function() {
-					$('#musician-1 .mem-socials').css('display', 'none');
-			});
+		$('#musician-1 .member-description').hover(function() {
+				$('#musician-1 .mem-socials').css('display', 'block');
+		}, function() {
+				$('#musician-1 .mem-socials').css('display', 'none');
+		});
 
-			$('#musician-2 .member-description').hover(function() {
-					$('#musician-2 .mem-socials').css('display', 'block');
-			}, function() {
-					$('#musician-2 .mem-socials').css('display', 'none');
-			});
+		$('#musician-2 .member-description').hover(function() {
+				$('#musician-2 .mem-socials').css('display', 'block');
+		}, function() {
+				$('#musician-2 .mem-socials').css('display', 'none');
+		});
 
-			$('#musician-3 .member-description').hover(function() {
-					$('#musician-3 .mem-socials').css('display', 'block');
-			}, function() {
-					$('#musician-3 .mem-socials').css('display', 'none');
-			});			
+		$('#musician-3 .member-description').hover(function() {
+				$('#musician-3 .mem-socials').css('display', 'block');
+		}, function() {
+				$('#musician-3 .mem-socials').css('display', 'none');
+		});			
 
-			$('.members-carousel').owlCarousel({
-					loop: true, 
-					nav: true,
-					navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-					items: 1,
-			});
+		$('.members-carousel').owlCarousel({
+				loop: true, 
+				nav: true,
+				navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+				items: 1,
+		});
 
-			$('.concerts-carousel').owlCarousel({
-					loop: true,
-					nav: true,
-					navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-					items: 1,					
-			});
+		$('.concerts-carousel').owlCarousel({
+				loop: true,
+				nav: true,
+				navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+				items: 1,					
+		});
 
-
-			// Audio Player
+	  // Audio Player
       var myPlaylist = [{
         mp3:'../libs/audio-player/mix/audio/GreyMonday.mp3',
         title:'Grey Monday',
